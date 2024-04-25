43689a176c493a09544e68151906c96bcfd1cc47
Mr Mase <supercrab@gmail.com>
2024-04-25 22:59:37 +0100

    Fixed null reference exception when calling methods when begin has not been called

--- RemoteSerial.cpp
+++ RemoteSerial.cpp
@@ -5,7 +5,7 @@ void RemoteSerialClass::begin(AsyncWebServer *server, const char* url){
 	_ws = new AsyncWebSocket("/remoteserialws"); // this hardcoded as it is referenced by the webpage
 
 	_server->on(url, HTTP_GET, [](AsyncWebServerRequest *request){
-	   request->send_P(200, F("text/html"), WEBPAGE_HTML);   
+	   request->send_P(200, F("text/html"), HTML_REMOTE_SERIAL_PAGE);   
 	});
 
 	_ws->onEvent([this](AsyncWebSocket* server, AsyncWebSocketClient* client, AwsEventType type, void* arg, uint8_t* data, size_t len) {
@@ -30,16 +30,19 @@ void RemoteSerialClass::setIncomingMessageHandler(IncomingMessageHandler handler
 }
 
 size_t RemoteSerialClass::write(uint8_t m) {
+	if (_ws == NULL) return 0;
 	_ws->textAll((const char*) &m, 1);
 	return 1;
 }
 
 size_t RemoteSerialClass::write(const uint8_t* buffer, size_t size) {
+	if (_ws == NULL) return 0;
 	_ws->textAll((const char*) buffer, size);
 	return size;
 }
 
 void RemoteSerialClass::cleanupClients(){
+	if (_ws == NULL) return;
 	_ws->cleanupClients();
 }
 
